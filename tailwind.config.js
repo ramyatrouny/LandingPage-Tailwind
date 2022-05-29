e0232f27b09a2bae611bad64cff9442052621208
Rami Atrouni <ramiatrouni@gmail.com>
2022-05-29 16:38:26 +0300

    prepare the initial navigation menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
         brightRedSupLight: 'hsl(12, 88%,95%)',
         darkBlue: 'hsl(228, 39%, 23%)',
         darkGrayishBlue: 'hsl(227, 12%, 61%)',
-        veryDarkBlue: 'hsl(223, 12%, 13%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
         veryPaleRed: 'hsl(13, 100%, 96%)',
         veryLightGray: 'hsl(0, 0%, 98%)'
       }
